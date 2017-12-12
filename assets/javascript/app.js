dd4b99150564c6fe53025819981c01d3d6d77b1f
sandond77 <sandu@ucdavis.edu>
2017-12-11 22:34:07 -0800

    initial files created

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBQserZLQqOnOi4NxMRg8gWRPgKJ7fmNXc",
+    authDomain: "rps-multiplayer-5dbf3.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-5dbf3.firebaseio.com",
+    projectId: "rps-multiplayer-5dbf3",
+    storageBucket: "rps-multiplayer-5dbf3.appspot.com",
+    messagingSenderId: "685934621948"
+  };
+  firebase.initializeApp(config);
