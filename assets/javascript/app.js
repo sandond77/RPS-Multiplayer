ba4f5884ace6274613554c19cca1515f8dc7d5ff
sandond77 <sandu@ucdavis.edu>
2017-12-13 23:24:10 -0800

    started implementing firebase player detection

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,8 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
+var player1Id = "";
+var player2Id = "";
 var player1Name = "";
 var player2Name = "";
 var numberOfPlayers = 0;
@@ -21,35 +23,42 @@ firebase.auth().signInAnonymously().catch(function(error) {
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    console.log("User is logged in")
-    console.log(user.uid);
-    database.ref().push("test");
+    if (player1Id === ""){
+      console.log("Player 1 is logged in")
+      player1Id = user.uid
+      console.log(player1Id);
 
-    $("#submitname").on("click", function(event) {
-      event.preventDefault();
+      $("#submitname").on("click", function(event) {
+        event.preventDefault();
 
-      var newPlayer = $("#name").val().trim();
+        var newPlayer = $("#name").val().trim();
 
-      if (newPlayer === "") {
-        alert("You did not enter anything")
-      } else if (player1Name === ""){
-          player1Name = newPlayer;
-          $("#player1").text(player1Name);
-          buttonMaker("#player1choice")
-          $("#player1result").empty();
-          $("#gamestatus").html("Waiting for Player 2");
-      } else if (player1Name !== "" && player2Name === ""){
-          player2Name = newPlayer;
-          $("#player2").text(player2Name);
-          buttonMaker("#player2choice")
-          $("#player2result").empty();
-          $("#gamestatus").html("Waiting for players to choose");
-      } else if (player1Name !== "" && player2Name !== ""){
-        alert("There are already two players. Please wait for one to leave.")
-      }
+        if (newPlayer === "") {
+          alert("You did not enter anything")
+        } else if (player1Name === ""){
+            player1Name = newPlayer;
+            $("#player1").text(player1Name);
+            buttonMaker("#player1choice")
+            $("#player1result").empty();
+            $("#gamestatus").html("Waiting for Player 2");
+        } else if (player1Name !== "" && player2Name === ""){
+            player2Name = newPlayer;
+            $("#player2").text(player2Name);
+            buttonMaker("#player2choice")
+            $("#player2result").empty();
+            $("#gamestatus").html("Waiting for players to choose");
+        } else if (player1Name !== "" && player2Name !== ""){
+          alert("There are already two players. Please wait for one to leave.")
+        }
 
-      $("#name").val('');
-    });
+        $("#name").val('');
+      });
+
+    } else if (player1Id !== "" && player2Id === ""){
+      console.log("Player 2 is logged in")
+      player2Id = user.uid
+      console.log(player2Id);
+    }
 
   } else {
     // No user is signed in.
@@ -96,6 +105,7 @@ function buttonMaker(arg){
 
 //Things to Do
 //Figure out how firebase users works and get it so that each user writes to player 1/player 2 respectively
+    //Setting player IDs to two different variables and saving them to firebase upon connection. When a user disconnects, the variables are emptied. Need firebase to push/pull variable info dynamically
 //Compare player 1 and player 2 choices to see who wins 
 //Code output of game
 //Code chatroom function and possible previous choices?
